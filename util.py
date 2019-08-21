23052313df3ece8b6be0e65adeed711b571172b3
bkwoka <bkwoka1@gmail.com>
2019-08-21 13:46:42 +0000

    Add new answer commit in data_mamger and sort_data_by_time in util

--- util.py
+++ util.py
@@ -1,6 +1,15 @@
 from datetime import datetime
 
-def time_convert(timestamp):
-    return datetime.fromtimestamp(int(timestamp)).strftime("%Y-%m-%d  %H:%M")    
-#
 
+def time_convert(timestamp):
+    return datetime.fromtimestamp(int(timestamp)).strftime("%Y-%m-%d  %H:%M")
+
+
+def sort_data_by_time(records):
+    """
+        This function sort by time input data
+    :param records: iterable object
+    :return: sorted iterable object
+    """
+    sort_data = sorted(records, key=lambda time: int(time['submission_time']), reverse=True)
+    return sort_data
